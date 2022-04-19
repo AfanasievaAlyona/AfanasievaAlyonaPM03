df377698d6bc38a97f29dfce58560706b055f628
stu-pksp118 <stu-pksp118@AMDVLA3511GW04>
2022-04-19 11:36:03 +0300

    Элементы

--- Afanasieva03/Program.cs
+++ Afanasieva03/Program.cs
@@ -8,11 +8,39 @@ namespace Afanasieva03
 {
     class Program
     {
-        int[] Autos;
+		struct Auto 
+		{
+			string mark;
+			string model;
+			int cum;
+			int size;
+			Tour* next;
+		};
         static void Main(string[] args)
         {
-            
+            int[] Autos = new  int[];
+            int n;
+			cout << "Введите массив: " << n << endl;
+			if (!n) 
+			{
+				cout << "Не введено число";
+			}
+			else 
+			{
+				int i = 0;
+				write (n) 
+				{
+					cout << "Автомобиль " << i + 1 << endl;
+					cout << "Марка " << i + 1 << n->mark << endl;
+				cin >> Autos[i].mark;
 
+				cout << "Модель " << i + 1 << n->model << endl;
+				cin >> Autos[i].model;
+
+				cout << "Цена " << i + 1 <<n-> cum << endl;
+				cin >> Autos[i].cum;
+				}
+			}
         }
     }
 }
